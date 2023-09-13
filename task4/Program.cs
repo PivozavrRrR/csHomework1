469561e9e233a1ddb8ed06a22e4e8180075244b4
Пивозавр <da_net2019@mail.ru>
2023-09-13 13:55:49 +0300

    task 4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+int startNum = 1;
+while(startNum <= num)
+{
+    if(startNum % 2 == 0)
+    {
+        Console.WriteLine(startNum);
+        startNum++;
+    }
+    else
+    {
+        startNum++;
+    }
+}
