e881d7b1c551b8d3112951f1a7a00fdd13a2c78c
Пивозавр <da_net2019@mail.ru>
2023-09-20 01:05:13 +0300

    task3 hw2

---
+++ homeWork2/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели, и проверяет, является ли этот день выходным.*/
+Console.Write("Enter number : ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+if(num == 6 || num == 7)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
