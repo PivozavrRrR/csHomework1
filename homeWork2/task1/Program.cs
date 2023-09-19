a416b40d9b5059f8dfd631a1f9ed00cf9b6e2664
Пивозавр <da_net2019@mail.ru>
2023-09-20 00:00:04 +0300

    task1 hw2

---
+++ homeWork2/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа*/
+Random rand = new Random();
+int num = rand.Next(100,1000);
+System.Console.WriteLine(num);
+    int num1 = num / 100;
+    int num3 = num % 10; 
+    int num2 = (num - num1*100 - num3) / 10;
+    System.Console.WriteLine(num2);
