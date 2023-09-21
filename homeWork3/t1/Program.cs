924190bb77c8b7ccdde8da8afa363057fe5af61a
Пивозавр <da_net2019@mail.ru>
2023-09-21 17:07:31 +0300

    hw3

---
+++ homeWork3/t1/Program.cs
@@ -0,0 +1,49 @@
+﻿﻿// Внутри класса Answer напишите метод IsPalindrome, который принимает на вход пятизначное число number и проверяет, является ли оно палиндромом.
+// Метод должен проверить является ли число пятизначным, в противном случае - вывести Число не пятизначное и False в следующей строке.
+// Для остальных чисел вернуть True или False.
+
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number)
+    {
+    // Введите свое решение ниже
+    if (number > 9999 && number < 100000)
+    {
+      if (number / 10000 == number % 10 && number / 1000 % 10 == number % 100 / 10)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    else
+    {
+      Console.WriteLine("Число не пятизначное");
+      return false;
+    }
+  }
+
+  // Не удаляйте и не меняйте метод Main! 
+  static public void Main(string[] args) 
+    {
+        int number;
+
+        if (args.Length >= 1) 
+        {
+            number = int.Parse(args[0]);
+        } 
+        else 
+        {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
---
+++ homeWork3/t2/Program.cs
@@ -0,0 +1,50 @@
+﻿﻿// // Внутри класса Answer напишите метод DistanceBetweenPoints, 
+// // который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, 
+// // и возвращает расстояние между ними в 3D пространстве.
+// // A (3,6,8); B (2,1,-7) -> 15.84
+// // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+        // Введите свое решение ниже
+        int x1 = pointA[0];
+        int x2 = pointA[1];
+        int x3 = pointA[2];
+        int y1 = pointB[0];
+        int y2 = pointB[1];
+        int y3 = pointB[2];
+
+        double Length = Math.Sqrt(Math.Pow((x1 - y1), 2) + Math.Pow((x2 - y2), 2) + Math.Pow((x3 - y3), 2));
+
+        return Length;
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
